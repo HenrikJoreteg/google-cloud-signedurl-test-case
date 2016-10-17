84290918e7b82dd8c1f22ffcd2c7cdc06b08d334
Philip Roberts <phil@latentflip.com>
2016-10-17 11:46:16 +0100

    Add content-type

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const PROJECT_ID = 'cowork-group'
-const BUCKET_NAME = 'cowork-group'
+const PROJECT_ID = 'lofty-hall-146709'
+const BUCKET_NAME = 'lofty-hall-146709.appspot.com'
 const KEY_FILE_PATH = 'keyfile.json'
 
 
@@ -16,14 +16,21 @@ const bucket = gcs.bucket(BUCKET_NAME)
 
 app.get('/url', (req, res) => {
   const file = req.query.file
+  const type = req.query.type
   if (!file) {
     res.code(400)
     res.send('include filename as \'file\' query param')
     return
   }
+  if (!type) {
+    res.code(400)
+    res.send('include content-type as \'type\' query param')
+    return
+  }
   bucket.file(file).getSignedUrl({
     action: 'write',
-    expires: Date.now() + 6000000
+    expires: Date.now() + 6000000,
+    contentType: type
   }, (error, signedUrl) => {
     if (error == null) {
       res.send(signedUrl)
